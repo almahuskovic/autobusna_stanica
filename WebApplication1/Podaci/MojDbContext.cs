685903d4f377de9d0fc0e2ae9d9e7c6ac5d862ef
almahuskovic <alma.huskovic@edu.fit.ba>
2020-11-30 00:42:20 +0100

    nuget paketi, mojdbcontext, obavijest controller, i view prikaz

---
+++ WebApplication1/Podaci/MojDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1;
+
+namespace Podaci
+{
+    public class MojDbContext:DbContext
+    {
+        public DbSet<ObavijestKategorija> ObavijestKategorija { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost; Database=BUS_STANICA;
+                        Trusted_Connection=true; MultipleActiveResultSets=true;");
+        }
+    }
+}
