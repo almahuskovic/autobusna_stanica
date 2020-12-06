9e43591c7b9cd04fc878bf582a5c3b16f547749b
almahuskovic <alma.huskovic@edu.fit.ba>
2020-12-06 19:39:14 +0100

    kategorijaObavijestPrikaz

--- WebApplication1/WebApplication1/Controllers/ObavijestKategorijaController.cs
+++ WebApplication1/WebApplication1/Controllers/ObavijestKategorijaController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Podaci;
 
 namespace WebApplication1.Controllers
 {
     public class ObavijestKategorijaController : Controller
     {
+        MojDbContext db = new MojDbContext();
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult Prikaz(int ObavijestID)
+        {
+            var m = db.ObavijestKategorija.ToList();
             return View();
         }
     }
