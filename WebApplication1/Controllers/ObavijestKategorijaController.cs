8e593971a9ab77e20c0c596a3b849b710631d85a
almahuskovic <alma.huskovic@edu.fit.ba>
2020-12-10 10:41:47 +0100

    Prikaz, Uredi, Dodaj, Obrisi Obavijest Kategorija

--- WebApplication1/Controllers/ObavijestKategorijaController.cs
+++ WebApplication1/Controllers/ObavijestKategorijaController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Podaci;
+using WebApplication1.Models;
+using WebApplication1.Models.ObavijestKategorija;
 
 namespace WebApplication1.Controllers
 {
@@ -12,8 +14,55 @@ namespace WebApplication1.Controllers
         MojDbContext db = new MojDbContext();
         public IActionResult Prikaz()
         {
-            var m = db.ObavijestKategorija.ToList();
+            var m = db.ObavijestKategorija.
+                Select(ob => new ObavijestKategorijaPrikazVM
+                {
+                    ObavijestID = ob.ObavijestID,
+                    Naziv = ob.Naziv
+                })
+                .ToList();
             return View(m);
+        }
+        public IActionResult Uredi(int ObavijestID)
+        {
+            ObavijestKategorijaUrediVM m;
+            if (ObavijestID == 0)
+            {
+                m = new ObavijestKategorijaUrediVM();
+            }
+            else
+            {
+                m = db.ObavijestKategorija.Where(o => o.ObavijestID == ObavijestID)
+               .Select(ob => new ObavijestKategorijaUrediVM
+               {
+                   Naziv = ob.Naziv
+               }).Single();
+            }
+
+            return View(m);
+        }
+        public IActionResult Snimi(ObavijestKategorijaUrediVM x)
+        {
+            ObavijestKategorija m;
+            if (x.ObavijestID == 0)
+            {
+                m = new ObavijestKategorija();
+                db.Add(m);
+            }
+            else
+            {
+                m = db.ObavijestKategorija.Where(o => o.ObavijestID == x.ObavijestID).SingleOrDefault();
+            }
+            m.Naziv = x.Naziv;
+            db.SaveChanges();
+            return Redirect("/ObavijestKategorija/Prikaz");
+        }
+        public IActionResult Obrisi(int ObavijestID)
+        {
+            var m=db.ObavijestKategorija.Find(ObavijestID);
+            db.Remove(m);
+            db.SaveChanges();
+            return Redirect("/ObavijestKategorija/Prikaz");
         }
     }
 }
