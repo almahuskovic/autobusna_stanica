d9b051530e85b50516e8df34587e2100e8490972
dedicamra <amra.dedic@edu.fit.ba>
2020-12-25 13:22:52 +0100

    Vozilo: dodavanje atributa OznakaVozila; izmjena u prikazu

---
+++ Podaci/Migrations/20201225113910_vozilo_atributOznaka.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Podaci.Migrations
+{
+    public partial class vozilo_atributOznaka : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OznakaVozila",
+                table: "Vozilo",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OznakaVozila",
+                table: "Vozilo");
+        }
+    }
+}
