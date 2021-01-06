1b25eb93fa8bab23c74fc446255251c761e3b551
dedicamra <amra.dedic@edu.fit.ba>
2021-01-06 18:52:28 +0100

    CRUD Cijena

--- Podaci/Data/ApplicationDbContext.cs
+++ Podaci/Data/ApplicationDbContext.cs
@@ -25,5 +25,6 @@ namespace WebApplication1.Data
         public DbSet<VrstaPopusta> VrstaPopusta{ get; set; }
         public DbSet<Karta> Karta{ get; set; }
         public DbSet<Stajalista> Stajalista{ get; set; }
+        public DbSet<Cijena> Cijena { get; set; }
     }
 }
---
+++ Podaci/Data/Migrations/20210106150806_cijena.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Data.Migrations
+{
+    public partial class cijena : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cijena",
+                columns: table => new
+                {
+                    CijenaID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GradPolaskaGradID = table.Column<int>(nullable: false),
+                    GradDolaskaGradID = table.Column<int>(nullable: false),
+                    JednosmijernaKartaCijena = table.Column<float>(nullable: false),
+                    PovratnaKartaCijena = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cijena", x => x.CijenaID);
+                    table.ForeignKey(
+                        name: "FK_Cijena_Grad_GradDolaskaGradID",
+                        column: x => x.GradDolaskaGradID,
+                        principalTable: "Grad",
+                        principalColumn: "GradID",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Cijena_Grad_GradPolaskaGradID",
+                        column: x => x.GradPolaskaGradID,
+                        principalTable: "Grad",
+                        principalColumn: "GradID",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cijena_GradDolaskaGradID",
+                table: "Cijena",
+                column: "GradDolaskaGradID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cijena_GradPolaskaGradID",
+                table: "Cijena",
+                column: "GradPolaskaGradID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cijena");
+        }
+    }
+}
